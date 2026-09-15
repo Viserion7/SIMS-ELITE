042fb2cb2ea68e17fd19a87acfde4f3ad9fafb03
Viserion7 <yannick0.0@gmx.net>
2026-09-15 08:45:57 +0200

    Einbdingung von OT in sims-Identidy

--- sims-identidy/Program.cs
+++ sims-identidy/Program.cs
@@ -1,6 +1,12 @@
 
 namespace sims_identidy;
+
 using Scalar.AspNetCore;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 public class Program
 {
@@ -14,6 +20,53 @@ public class Program
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
 
+        string serviceName = "sims-identidy";
+        IConfigurationSection otlpEndpoint = builder.Configuration.GetSection("OTEL_EXPORTER_OTLP_ENDPOINT");
+        string? endpointUrl = otlpEndpoint["url"];
+        string? endpointKey = otlpEndpoint["key"];
+
+        builder.Logging.AddConsole();
+        if (!string.IsNullOrEmpty(endpointUrl))
+        {
+            //tracing
+            builder.Services.AddOpenTelemetry()
+                .WithTracing(tpb =>
+                {
+                    tpb.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName))
+                       .AddAspNetCoreInstrumentation()
+                       .AddHttpClientInstrumentation()
+                       .AddOtlpExporter(otlp =>
+                       {
+                           otlp.Endpoint = new Uri(endpointUrl);
+                           if (!string.IsNullOrEmpty(endpointKey))
+                           {
+                               otlp.Headers = "signoz-ingestion-key=" + endpointKey;
+                           }
+                       });
+                });
+
+
+
+            //logging
+            builder.Logging.AddOpenTelemetry(opt =>
+            {
+                opt.IncludeFormattedMessage = true;
+                opt.IncludeScopes = true;
+                opt.ParseStateValues = true;
+                opt.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName));
+                opt.AddOtlpExporter(otlp =>
+                {
+                    otlp.Endpoint = new Uri(endpointUrl);
+                    if (!string.IsNullOrEmpty(endpointKey))
+                    {
+                        otlp.Headers = "signoz-ingestion-key=" + endpointKey;
+                    }
+
+                });
+            });
+
+        }
+
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
@@ -22,6 +75,7 @@ public class Program
             app.MapScalarApiReference();
             app.MapOpenApi();
         }
+
 
         app.UseHttpsRedirection();
 
