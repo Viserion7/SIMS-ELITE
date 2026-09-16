c5cf736d44f42e775e8d224718f1671d3c9423c1
Viserion7 <yannick0.0@gmx.net>
2026-09-16 15:06:05 +0200

    User Controler mit DTOS gemacht

--- sims-identity/Controllers/UsersController.cs
+++ sims-identity/Controllers/UsersController.cs
@@ -5,6 +5,7 @@ namespace sims_identity.Controllers;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Microsoft.VisualBasic;
 using sims_identity.Data;
 
 
@@ -23,8 +24,16 @@ public class UserController : ControllerBase
 
 
     [HttpPost]
-    public async Task<ActionResult<User>> CreateUser(User user)
+    public async Task<ActionResult<CreateUserDto>> CreateUser(CreateUserDto incommingUser)
     {
+        User user = new User
+        {
+            email = incommingUser.email,
+            password_hash = incommingUser.password, // TO Do noch pw Hash einabuen
+            is_deleted = false
+        };
+
+
         _context.User.Add(user);
 
         await _context.SaveChangesAsync();
@@ -33,9 +42,16 @@ public class UserController : ControllerBase
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<User>> GetUser(int id)
+    public async Task<ActionResult<UserDto>> GetUser(int id)
     {
-        var user = await _context.User.FindAsync(id);
+        var user = await _context.User.
+            Where(user => user.id == id)
+            .Select(user => new UserDto
+            {
+                id = user.id,
+                email = user.email,
+                is_deleted = user.is_deleted
+            }).FirstOrDefaultAsync();
 
         if (user == null)
             return NotFound();
@@ -51,13 +67,16 @@ public class UserController : ControllerBase
 
 
     [HttpPut("{id}")]
-    public async Task<ActionResult> UpdateUser(int id, User updatedUser)
+    public async Task<ActionResult> UpdateUser(int id, UpdateUserDto updatedUser)
     {
 
+
         var userEntity = await _context.User.FindAsync(id);
+        if (userEntity == null)
+            return NotFound();
 
         userEntity.email = updatedUser.email;
-        userEntity.password_hash = updatedUser.password_hash;
+        userEntity.password_hash = updatedUser.password; // To Do hash Funktion noch einbauen
         userEntity.is_deleted = updatedUser.is_deleted;
 
         _context.Entry(userEntity).State = EntityState.Modified;
@@ -94,6 +113,19 @@ public class UserController : ControllerBase
         }
 
 
+    }
+    [HttpGet("{id}/details")]
+    public async Task<ActionResult<User>> GetDetails(int id)
+    {
+        var user = await _context.User
+    .Include(user => user.Levels)
+        .ThenInclude(level => level.Categorys)
+    .FirstOrDefaultAsync(user => user.id == id);
+
+        if (user == null)
+            return NotFound();
+
+        return user;
     }
 
 }
