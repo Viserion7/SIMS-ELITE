e65b89bf7bb15ab24f66409d7515b1549c749720
Viserion7 <yannick0.0@gmx.net>
2026-09-16 15:05:53 +0200

    categorie schnittpunkt fertig

---
+++ sims-identity/Controllers/Categories.cs
@@ -0,0 +1,35 @@
+//inspiration: https://medium.com/@aschultzme/introduction-to-entity-framework-building-a-simple-crud-api-in-net-b22eb2efbe6e
+using Microsoft.AspNetCore.Mvc;
+
+namespace sims_identity.Controllers;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualBasic;
+using sims_identity.Data;
+
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class CategoriesController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+    private readonly ILogger<CategoriesController> _logger;
+
+    public CategoriesController(ApplicationDbContext context, ILogger<CategoriesController> logger)
+    {
+        _context = context;
+        _logger = logger;
+    }
+
+
+
+    [HttpGet("categories")]
+    public async Task<ActionResult<List<Category>>> GetAllCategories()
+    {
+        return _context.Category.ToList();
+    }
+
+
+
+}
