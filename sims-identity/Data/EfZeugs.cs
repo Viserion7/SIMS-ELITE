b87210732fa27a6360ca943130b1355c76fd4e4d
Viserion7 <yannick0.0@gmx.net>
2026-09-16 13:40:04 +0200

    Mit man mit EWf diese klassen sprechen kann Kontext

--- sims-identity/Data/EfZeugs.cs
+++ sims-identity/Data/EfZeugs.cs
@@ -7,4 +7,8 @@ public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
     public DbSet<User> User { get; set; }
+    public DbSet<Category> Category { get; set; }
+
+    public DbSet<Level> Level { get; set; }
+
 }
