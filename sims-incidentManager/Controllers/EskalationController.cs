0fc3426d4590118b53390d74cdf29bf7fd8a30f2
Tobias Vonmetz <is251001@ustp-students.at>
2026-09-18 09:56:44 +0200

    skeleton for inci added

---
+++ sims-incidentManager/Controllers/EskalationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace sims_incidentManager.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class EskalationController
+    {
+        private readonly ILogger<EskalationController> logger;
+        private readonly HttpClient httpClient;
+
+        public EskalationController(ILogger<EskalationController> logger, HttpClient httpClient)
+        {
+            this.logger = logger;
+            this.httpClient = httpClient;
+        }
+    }
+}
---
+++ sims-incidentManager/Model/Eskalation.cs
@@ -0,0 +1,9 @@
+﻿namespace sims_incidentManager.Model
+{
+    public class Eskalation
+    {
+        public Guid id { get; set;  }
+        public string message { get; set; } = "";
+        public int id_user { get; set; }
+    }
+}
